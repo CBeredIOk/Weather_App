dcb82fe5dabcefcdfc7c4337eaa5c272121621c8
CBeredIOk <CBeredIOk@yandex.ru>
2023-10-12 16:06:06 +0300

    init commit

---
+++ settings.py
@@ -0,0 +1,4 @@
+
+API_GET_REQUEST_WEATHER_IN_CITY = "http://api.openweathermap.org/data/2.5/weather?q={location}&appid={api_key}&lang=ru"
+API_KEY = "6548db94a158b9741342a504182e254b"
+TO_CELSIUS = 273.15
